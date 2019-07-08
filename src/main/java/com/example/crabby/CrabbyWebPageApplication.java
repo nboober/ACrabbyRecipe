d5d40085f1029e525e1a5d9f675a632583f0b500
nboober <nboober@terpmail.umd.edu>
2019-07-08 11:32:55 -0400

    Initial Commit

---
+++ src/main/java/com/example/crabby/CrabbyWebPageApplication.java
@@ -0,0 +1,13 @@
+package com.example.crabby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrabbyWebPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrabbyWebPageApplication.class, args);
+	}
+
+}
